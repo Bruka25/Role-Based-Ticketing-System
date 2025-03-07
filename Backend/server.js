e478236fddca6b1f33339dfc979ad0bfa74eab66
Bruka25 <bkteshome862@gmail.com>
2025-03-07 22:02:44 +0300

    add the server module

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
